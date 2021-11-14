1455123e2e49f00ef925746d820ad08904fb0a37
Mateo Craviotto <matecraviotto@gmail.com>
2021-11-13 21:15:23 -0300

    Agrego homepage y añado bootstrap, archivo styles.scss

--- ecommerce/app/javascript/packs/application.js
+++ ecommerce/app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'bootstrap'
+import 'css/styles'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
