d90121dfdfa20118f1e434efc47d28597eb49109
Mateo Craviotto <macraviotto@fi.uba.ar>
2021-11-24 01:55:02 -0300

    Agrego foto de perfil para el usuario, gema image_processing

--- ecommerce/app/controllers/application_controller.rb
+++ ecommerce/app/controllers/application_controller.rb
@@ -5,9 +5,8 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :avatar])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :avatar])
     end
     
-
 end
