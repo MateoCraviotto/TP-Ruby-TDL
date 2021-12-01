c070462c15a9a0379b6839ddb746a56d0e17b19f
Melina Lazzaro <melinalazzaro01@gmail.com>
2021-11-30 21:24:38 -0300

    Currency exchange
    Co-Authored-By: Fer <68122064+ferbalmaceda23@users.noreply.github.com>

--- ecommerce/app/models/car.rb
+++ ecommerce/app/models/car.rb
@@ -1,5 +1,12 @@
+require "open_exchange_rates"
+
 class Car < ApplicationRecord
     has_one_attached :car_image
     belongs_to :user
     has_many :questions
+
+    def exchange_price(currency)
+        exchange = OpenExchangeRates::Rates.new(:app_id => "d048914d53b24d148811eb19ebaa047b")
+        return exchange.convert(self.price, :from => "USD", :to => currency)
+    end
 end
